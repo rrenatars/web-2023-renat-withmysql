6f7c7fe17cbe9a425eb6809242320457a0dc6640
rrenatars <renat.arslanov2015@yandex.ru>
2023-04-01 16:24:47 +0300

    fix

--- data/sql/posts/posts.sql
+++ data/sql/posts/posts.sql
@@ -7,7 +7,7 @@ INSERT INTO
 		publish_date,
         image_url,
 		featured
-) 
+	) 
 VALUES (
 	'The Road Ahead',
 	'The road ahead might be paved - it might not be.',
@@ -27,7 +27,7 @@ INSERT INTO
 		publish_date,
         image_url,
 		featured
-) 
+	) 
 VALUES (
 	'From Top Down',
 	'Once a year, go someplace you’ve never been before.',
@@ -47,7 +47,7 @@ INSERT INTO
 		publish_date,
         image_url,
 		featured
-) 
+	) 
 VALUES (
 	'Still Standing Tall',
 	'Life begins at the end of your comfort zone.',
@@ -67,7 +67,7 @@ INSERT INTO
 		publish_date,
         image_url,
 		featured
-) 
+	) 
 VALUES (
 	'Sunny Side Up',
 	'No place is ever as bad as they tell you it’s going to be.',
@@ -87,7 +87,7 @@ INSERT INTO
 		publish_date,
         image_url,
 		featured
-) 
+	) 
 VALUES (
 	'Water Falls',
 	'We travel not to escape life, but for life not to escape us.',
@@ -107,7 +107,7 @@ INSERT INTO
 		publish_date,
         image_url,
 		featured
-) 
+	) 
 VALUES (
 	'Through the Mist',
 	'Travel makes you see what a tiny place you occupy in the world.',
@@ -127,7 +127,7 @@ INSERT INTO
 		publish_date,
         image_url,
 		featured
-) 
+	) 
 VALUES (
 	'Awaken Early',
 	'Not all those who wander are lost.',
@@ -147,7 +147,7 @@ INSERT INTO
 		publish_date,
         image_url,
 		featured
-) 
+	) 
 VALUES (
 	'Try it Always',
 	'The world is a book, and those who do not travel read only one page.',
